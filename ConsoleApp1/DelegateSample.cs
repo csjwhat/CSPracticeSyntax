008cecbc355204c43dfd55717c71aab2f5399ce1
csjwhat <csjwhats@gmail.com>
2017-09-28 09:38:48 +0900

    C#ソースの登録

---
+++ ConsoleApp1/DelegateSample.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class DelegateSample
+    {
+
+        // Delegate
+        public delegate int Keisan(int x, int y);
+
+        static void Main22(string[] args)
+        {
+            DelegateSample dl = new DelegateSample();
+            dl.execute2();
+
+            // 拡張メソッドを試す
+            Console.WriteLine("aaa" + StringExc.AddInfo("aaa"));
+
+            "aaa".AddInfoString();
+
+            Console.WriteLine(dl.DelegateSampleAddMethod());
+
+            Console.ReadLine();
+
+            List<string> list = new List<string> { "Tatsuya", "akiko", "ai", "hiroshi", "yoko" };
+            IEnumerable<string> q =
+                from p in list
+                where p.Length < 5
+                select p;
+
+            IEnumerable<string> q2 = list.Where(p => (p.Length <= 5)).Select(p => p);
+
+
+            foreach (var x in q)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+        }
+
+        private string whereClouse(string p)
+        {
+            if (p.Length <= 5)
+            {
+                return p;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void execute()
+        {
+            List<Keisan> keisan = new List<Keisan>();
+            keisan.Add(Tashizan);
+            keisan.Add(Hikizan);
+            keisan.Add(Kakezan);
+            keisan.Add(Warizan);
+
+            //PrintKeisan(keisan);
+        }
+
+        private static void PrintKeisan(List<Func<int, int, int>> keisan)
+        {
+            foreach (var k in keisan)
+            {
+                int r = k(6, 3);
+                Console.WriteLine(r);
+                Console.ReadLine();
+            }
+        }
+
+        private int Tashizan(int a, int b) { return a + b; }
+        private int Hikizan(int a, int b) { return a - b; }
+        private int Kakezan(int a, int b) { return a * b; }
+        private int Warizan(int a, int b) { return a / b; }
+
+        public void execute2()
+        {
+            List<Func<int, int, int>> keisan = new List<Func<int, int, int>>();
+            keisan.Add((a, b) => (a + b));
+            keisan.Add((a, b) => (a - b));
+            keisan.Add((a, b) => (a * b));
+            keisan.Add((a, b) => (a / b));
+            PrintKeisan(keisan);
+        }
+    }
+
+    static class StringExc
+    {
+        public static string AddInfo(string value)
+        {
+            return value + ":AddInfo";
+        }
+
+        public static string AddInfoString(this string value)
+        {
+            return value + ":AddInfoString";
+        }
+
+        public static string DelegateSampleAddMethod(this DelegateSample value)
+        {
+            return "delegateSampleAddMethod";
+        }
+    }
+
+    public class TestAsync
+    {
+        static async Task OutputStringAs(string value)
+        {
+            Action a = () => { Console.WriteLine("aaaaaa"); };
+            await Task.Run(a);
+        }
+    }
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sex = new List<Sex> {
+                new Sex { Id = 0, Text = "未登録" },
+                new Sex { Id = 1, Text = "男性" },
+                new Sex { Id = 2, Text = "女性" }
+               };
+
+            var member = new List<Member>
+            {
+                new Member {Id = 1, Name="Tatsuya", SexId = 1},
+                new Member {Id = 2, Name="Akiko", SexId = 2},
+                new Member {Id = 3, Name="Ai", SexId = 0},
+                new Member {Id = 4, Name="Hiroshi", SexId = 1},
+                new Member {Id = 5, Name="Yoko", SexId = 2}
+            };
+
+            var q =
+                from m in member
+                join s in sex on m.SexId equals s.Id
+                let sid = m.SexId
+                where m.SexId > 0
+                orderby m.Name descending
+                select
+                new { Text = $"{m.Id}:{m.Name}({s.Text}){sid}", Sex = s.Text } into v
+                group v by v.Sex;
+
+            foreach(var g in q)
+            {
+                Console.WriteLine("---------------------------------------");
+                Console.WriteLine(g.Key);
+                Console.WriteLine("---------------------------------------");
+                foreach(var x in g)
+                {
+                    Console.WriteLine(x.Text);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+
+
+    class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int SexId { get; set; }
+    }
+
+    class Sex
+    {
+        // Property
+        public int Id { get; set; } // Id
+        public string Text { get; set; } // 性別（文字列）
+    }
+
+}
